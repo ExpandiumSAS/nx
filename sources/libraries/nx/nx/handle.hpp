51bf5aa13c9b9c5d2b0ea26498261d7b50585598
Remy Chibois <remy.chibois@expandium.com>
2015-03-23 19:00:42 +0100

    initial import

---
+++ sources/libraries/nx/nx/handle.hpp
@@ -0,0 +1,16 @@
+#ifndef __NX_HANDLE_H__
+#define __NX_HANDLE_H__
+
+#include <nx/config.h>
+
+namespace nx {
+
+class NX_API handle
+{
+public:
+private:
+};
+
+} // namespace nx
+
+#endif // __NX_HANDLE_H__
