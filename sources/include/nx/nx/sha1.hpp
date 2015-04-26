4753a8ff4da1117a574aa9aa2ea7fbc773198dc1
Remy Chibois <remy.chibois@expandium.com>
2015-04-26 10:56:13 +0200

    added basen and sha1 utilities

---
+++ sources/include/nx/nx/sha1.hpp
@@ -0,0 +1,65 @@
+/*
+    sha1.h - header of
+
+    ============
+    SHA-1 in C++
+    ============
+
+    100% Public Domain.
+
+    Original C Code
+        -- Steve Reid <steve@edmweb.com>
+    Small changes to fit into bglibs
+        -- Bruce Guenter <bruce@untroubled.org>
+    Translation to simpler C++ Code
+        -- Volker Grabsch <vog@notjusthosting.com>
+*/
+
+#ifndef SHA1_HPP
+#define SHA1_HPP
+
+#include <stdint.h>
+
+#include <iostream>
+#include <string>
+
+#include <nx/config.h>
+
+namespace nx {
+
+class NX_API SHA1
+{
+public:
+    SHA1();
+
+    void update(const std::string& s);
+    void update(std::istream& is);
+
+    std::string final();
+
+    static std::string from_file(const std::string& filename);
+
+private:
+    // number of 32bit integers per SHA1 digest
+    static const std::size_t digest_ints = 5;
+    // number of 32bit integers per SHA1 block */
+    static const std::size_t block_ints = 16;
+    static const std::size_t block_bytes = block_ints * 4;
+
+    using block_type = uint32_t[block_bytes];
+    using int_block_type = uint32_t[block_ints];
+
+    void reset();
+    void transform(block_type block);
+
+    static void buffer_to_block(const std::string& buffer, block_type block);
+    static void read(std::istream& is, std::string& s, std::size_t max);
+
+    uint32_t digest_[digest_ints];
+    std::string buffer_;
+    uint64_t transforms_;
+};
+
+} // namespace nx
+
+#endif // SHA1_HPP
