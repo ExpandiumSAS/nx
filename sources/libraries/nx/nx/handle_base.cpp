ffa42d151c1e3762204de2dbc0c099d551385c76
Remy Chibois <remy.chibois@expandium.com>
2015-05-28 15:37:42 +0200

    removing ev-based implementation

--- sources/libraries/nx/nx/handle_base.cpp
+++
@@ -1,9 +0,0 @@
-#include <nx/handle_base.hpp>
-
-namespace nx {
-
-std::shared_ptr<handle_base>
-handle_base::ptr()
-{ return shared_from_this(); }
-
-} // namespace nx
--- sources/libraries/nx/nx/watcher_base.cpp
+++
@@ -1,9 +0,0 @@
-#include <nx/watcher_base.hpp>
-
-namespace nx {
-
-std::shared_ptr<watcher_base>
-watcher_base::ptr()
-{ return shared_from_this(); }
-
-} // namespace nx
