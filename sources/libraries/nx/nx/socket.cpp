17dd4b6d75153bfa3d256af1333c7a3e23d1a50b
Remy Chibois <remy.chibois@expandium.com>
2015-06-02 18:23:43 +0200

    updated filter model

--- sources/libraries/nx/nx/socket.cpp
+++ sources/libraries/nx/nx/socket.cpp
@@ -9,29 +9,58 @@ namespace nx {
 socket::socket(service& s)
 : s_(s),
 socket_(s_.io_service())
-{
-    auto self = shared_from_this();
-    s_.register(self);
-}
+{}
 
 socket::~socket()
 {}
 
 void
 socket::start()
-{}
+{
+    s_.register(shared_from_this());
+    read();
+}
 
 void
 socket::stop()
 {
-    close();
     s_.unregister(shared_from_this());
+    close();
 }
 
 void
 socket::read()
 {
+    auto self(shared_from_this());
 
+    socket_.async_read_some(
+        boost::asio::buffer(buffer_),
+        [this, self](boost::system::error_code ec, std::size_t count) {
+
+        if (!ec) {
+            request_parser::result_type result;
+          std::tie(result, std::ignore) = request_parser_.parse(
+              request_, buffer_.data(), buffer_.data() + bytes_transferred);
+
+          if (result == request_parser::good)
+          {
+            request_handler_.handle_request(request_, reply_);
+            do_write();
+          }
+          else if (result == request_parser::bad)
+          {
+            reply_ = reply::stock_reply(reply::bad_request);
+            do_write();
+          }
+          else
+          {
+            do_read();
+          }
+        } else if (ec != boost::asio::error::operation_aborted) {
+            connection_manager_.stop(shared_from_this());
+        }
+    }
+    );
 }
 
 void
