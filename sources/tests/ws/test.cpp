560ca27323e7f1a50d94cbc06a2eaa4805a5e226
Pierre Salmon <pierre.salmon@expandium.com>
2016-12-21 16:30:35 +0100

    chore: remove UT

--- sources/tests/ws/test.cpp
+++ sources/tests/ws/test.cpp
@@ -17,23 +17,23 @@ BOOST_AUTO_TEST_CASE(ws_server)
     req << header{ Sec_WebSocket_Key, accept_value };
     BOOST_CHECK_EQUAL("s3pPLMBiTxaQ9kYGzzhZRbK+xOo=", ws::server_challenge(req));
 
-    httpd hd;
-    hd(WS) / "echo" = ws_connection(
-        [](auto&& ctx, const buffer& data) {
-            std::string str(data.begin(), data.end());
-            ctx << ws_text << "reponse: " << str;
-        }
-    );
+    // httpd hd;
+    // hd(WS) / "echo" = ws_connection(
+    //     [](auto&& ctx, const buffer& data) {
+    //         std::string str(data.begin(), data.end());
+    //         ctx << ws_text << "reponse: " << str;
+    //     }
+    // );
 
-    cond_var cv;
+    // cond_var cv;
 
-    hd(GET) / "stop" = [&cv](const request& req, buffer& data, reply& rep) {
-        rep << "stop";
-        cv.notify();
-    };
+    // hd(GET) / "stop" = [&cv](const request& req, buffer& data, reply& rep) {
+    //     rep << "stop";
+    //     cv.notify();
+    // };
 
-    auto ep = make_endpoint("0.0.0.0", 1234);
-    hd(ep);
+    // auto ep = make_endpoint("0.0.0.0", 1234);
+    // hd(ep);
 
-    cv.wait();
+    // cv.wait();
 }
